783dd03ea3224aee9691cdb783ab2ef6d45a7f57
Ginger <kretschmons@gmail.com>
2015-10-16 13:15:35 -0700

    Add content to the readme and tweak the html format.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
   $(".btn").click(function(event) {
     var pinganswer = prompt("Enter a number between 1 and 100!")
   });
+
+  for (var i = 1; i <= 100; i++) {
+    var string = '';
+
+    if (i % 3 == 0) {
+      string += 'Ping';
+
+      if (i % 5 == 0) {
+        string += 'Pong';
+
+      if (string == '') {
+      string += i;
+
+    }
+  }
 });
