c33d5a221ac500403f9e628dfef8f4f2b1318bec
Ginger <kretschmons@gmail.com>
2015-10-16 21:07:58 -0700

    get the button to at least work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
 
-  $(".btn").click(function(event) {
-    var pinganswer = prompt("Enter a number between 1 and 100!")
+  $("button").click(function(event) {
+    prompt("Enter a number between 1 and 100!");
+
+   var pingpong = function () {
+      var i, list;
+      for (i = 1; i <= 101; i += 1) {
+        var list = '';
+
+        if (i % 15 == 0) {
+          list += 'PongPong'; }
+
+        if (i % 3 == 0) {
+          list += 'Ping'; }
+
+        if (i % 5 == 0) {
+          list += 'Pong'; }
+
+        if (list == '') {
+          list += i; } 
+
+    event.preventDefault();
   });
-
-  for (var i = 1; i <= 100; i++) {
-    var string = '';
-
-    if (i % 3 == 0) {
-      string += 'Ping';
-
-      if (i % 5 == 0) {
-        string += 'Pong';
-
-      if (string == '') {
-      string += i;
-
-    }
-  }
 });
