70e23e59e733f5afe9c97af60d23345af3eb8a7c
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-11-28 19:30:12 -0800

    update js file branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,22 @@
 $(document).ready(function() {
-
   $("button.btn").click(function(event) {
-    prompt("Enter a number between 1 and 100!");
-
-      var listItems = "";
-
-      var i;
-
-      for (i = 1; i < 101; i = i + 1) {
-
-        listItems += "<li>";
-
-        if ((i % 3 === 0) && (i % 5 ===0)) {
-          listItems += "PongPong";
-        }
-
-        else if (i % 3 === 0) {
-          listItems += "Ping";
-        }
-
-        else if (i % 5 === 0) {
-          listItems += "Pong";
-        }
-
-        else {
-          listItems += i;
-        }
-
-        listItems += "</li>";
-      }
-
-      $("#answer").show();
-
     event.preventDefault();
-
+    prompt("Enter a number between 1 and 100!");
+      var listItems = [];
+      for (var index = 1; index <= 100; index++) {
+        if ((i % 3 === 0) && (i % 5 ===0)) {
+          return index = "pongpong";
+        }
+        else if (i % 3 === 0) {
+          return index = "ping";
+        }
+        else if (i % 5 === 0) {
+          return index = "pong";
+        }
+        else {
+          return listItems.push(index);
+        }
+      }
+      $("#answer").show();
   });
 });
